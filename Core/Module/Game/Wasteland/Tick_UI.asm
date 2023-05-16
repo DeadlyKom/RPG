e449bfd478563e13d53d3cb1c0e9dc697377bb15
Mikhail Gamaev <gma.deadly.kom@mail.ru>
2023-05-16 10:32:15 +0300

    - рефактор логики UI

--- Core/Module/Game/Wasteland/Tick_UI.asm
+++ Core/Module/Game/Wasteland/Tick_UI.asm
@@ -9,48 +9,30 @@
 ; Note:
 ;   включить страницу работы с объектами
 ; -----------------------------------------
-Tick_UI:        ;
-                LD IX, PLAYER_ADR
+Tick_UI:        LD IX, PLAYER_ADR
+
+                ; анимационный тик прогресс баров здоровья
                 LD A, (IX + FObject.Character.Health)
                 LD HL, PlayerState.Health
-                SUB (HL)
-                JP P, $+5
-                LD A, #7F
-                INC L
-                INC L
-                LD (HL), A
-
-                LD IX, PLAYER_ADR
+                LD DE, (0x00 << 8) | HEALTH_BAR
+                CALL .Apply
+                ; анимационный тик прогресс баров бензака
                 LD A, (IX + FObject.Character.Gas)
                 LD HL, PlayerState.Gas
-                SUB (HL)
-                JP P, $+5
-                LD A, #7F
-                INC L
-                INC L
-                LD (HL), A
-
-                LD IX, PLAYER_ADR
+                LD DE, (0x02 << 8) | GAS_BAR
+                CALL .Apply
+                ; анимационный тик прогресс баров турбонаддува
                 LD A, (IX + FObject.Character.Turbo)
                 LD HL, PlayerState.Turbo
-                SUB (HL)
+                LD DE, (0x01 << 8) | TURBO_BAR
+
+.Apply          SUB (HL)
                 JP P, $+5
                 LD A, #7F
                 INC L
                 INC L
                 LD (HL), A
-                
-                ; анимационный тик прогресс баров
-                LD HL, PlayerState.Health + 3
-                LD DE, (0x01 << 8) | HEALTH_BAR
-                CALL Packs.OpenWorld.UI.Bar_Animation
-
-                LD HL, PlayerState.Gas + 3
-                LD DE, (0x02 << 8) | GAS_BAR
-                CALL Packs.OpenWorld.UI.Bar_Animation
-
-                LD HL, PlayerState.Turbo + 3
-                LD DE, (0x00 << 8) | TURBO_BAR
+                INC L
                 JP Packs.OpenWorld.UI.Bar_Animation
 
                 endif ; ~_MODULE_GAME_RENDER_WASTELAND_UI_BAR_TICK_
